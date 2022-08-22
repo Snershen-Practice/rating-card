f85e5182a60d149df3c9f2b666c783efc5f3f76c
Wu Jane <jane72418@gmail.com>
2022-08-22 14:53:15 +0800

    增加評分回饋

--- js/script.js
+++ js/script.js
@@ -3,17 +3,14 @@ const rate_card = document.querySelector("#rate-card");
 const thank_card = document.querySelector(".thank-card");
 let rating = document.querySelector(".rating");
 let result_num = document.querySelector(".result-num");
-let input = document.querySelector("input");
 submit_btn.addEventListener("click", () => {
   rate_card.style.display = "none";
   thank_card.style.display = "flex";
 });
 
 rating.addEventListener("click", (e) => {
-  console.log(e);
-  if (e.target.nodeName === "INPUT") {
-    result_num.textContent = input.value;
-    console.log(input.value);
+  if (e.target.nodeName == "INPUT") {
+    result_num.textContent = e.target.value;
   }
 });
 
